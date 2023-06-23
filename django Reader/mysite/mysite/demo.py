2c6c804d9d3d322d103bb2007bb85cdbf0672466
Sagar Chauhan <jrsagaroo7@gmail.com>
2023-06-23 11:06:48 +0530

    update

---
+++ django Reader/mysite/mysite/demo.py
@@ -0,0 +1,11 @@
+import sqlite3
+import pandas as pd
+
+df=pd.read_csv('synonyms.csv')
+df.columns=df.columns.str.strip()
+
+connection=sqlite3.connect('demo.db')
+df.to_sql('housing_development',connection, if_exists='replace')
+connection.close()
+
+
